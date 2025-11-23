835bf8029cd1b7a68f07a65839d164cfffaf3731
cesaredvvardo <elmoteroloco@gmail.com>
2025-11-23 16:49:02 -0300

    feat: permite la conexión a backend local y de producción

--- src/script.js
+++ src/script.js
@@ -7,7 +7,10 @@ const logo = document.getElementById("logo")
 
 let authToken = null
 let userRole = "guest"
-const API_URL = "http://localhost:3000/api"
+const API_URL =
+    window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+        ? "http://localhost:3000/api"
+        : "https://zombpocalypsis.vercel.app/api"
 let lastListedProducts = []
 let commandHistory = []
 let historyIndex = -1
